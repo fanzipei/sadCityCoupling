b062fb40f74e3a6bdf6331b9960d25337ae3d36a
fan <fanzipei@iis.u-tokyo.ac.jp>
2017-07-06 21:31:51 +0900

    discretize

--- dirichlet_process.py
+++ dirichlet_process.py
@@ -17,7 +17,7 @@ gamma = 0.005
 def load_sparse_matrix(filename, num_loc, is_gaussian):
     Ytd = np.zeros([num_time, num_u, num_loc], dtype=np.float) + 0.0005
     with open(filename, 'r') as f:
-        for uid_str, uidx_str, lidx_str, tidx_str in csv.reader(f):
+        for uidx_str, lidx_str, tidx_str in csv.reader(f):
             uidx = int(uidx_str)
             lidx = int(lidx_str)
             tidx = int(tidx_str) / 2
@@ -48,15 +48,31 @@ def get_matching(score):
 
 
 def dirichlet_process(P, XO, XT):
+    data_t = []
+    data_o = []
+    for m in xrange(6, 8):
+        for d in xrange(1, 32):
+            if m == 6 and d == 31:
+                continue
+            filename_t = '/media/fan/HDPC-UT/ZDC/TrainingForMapping/tokyo/sorted/2012{:02d}{:02d}.csv'.format(m, d)
+            print 'Reading {}'.format(filename_t)
+            data_t.append(load_sparse_matrix(filename_t, num_loc_t, True))
+            filename_o = '/media/fan/HDPC-UT/ZDC/TrainingForMapping/osaka/sorted/2012{:02d}{:02d}.csv'.format(m, d)
+            print 'Reading {}'.format(filename_o)
+            data_o.append(load_sparse_matrix(filename_o, num_loc_o, False))
+
+
     for i in xrange(max_iter):
         d = np.random.randint(num_day)
-        print 'Read {}th files'.format(d+1)
-        if d < 30:
-            Ytd = load_sparse_matrix('./tokyo_traj200/201206{:02d}.csv'.format(d+1), num_loc_t, True)
-            Yod = load_sparse_matrix('./osaka_traj200/201206{:02d}.csv'.format(d+1), num_loc_o, False)
-        else:
-            Ytd = load_sparse_matrix('./tokyo_traj200/201207{:02d}.csv'.format(d-29), num_loc_t, True)
-            Yod = load_sparse_matrix('./osaka_traj200/201207{:02d}.csv'.format(d-29), num_loc_o, False)
+        # print 'Read {}th files'.format(d+1)
+        # if d < 30:
+            # Ytd = load_sparse_matrix('./tokyo_traj200/201206{:02d}.csv'.format(d+1), num_loc_t, True)
+            # Yod = load_sparse_matrix('./osaka_traj200/201206{:02d}.csv'.format(d+1), num_loc_o, False)
+        # else:
+            # Ytd = load_sparse_matrix('./tokyo_traj200/201207{:02d}.csv'.format(d-29), num_loc_t, True)
+            # Yod = load_sparse_matrix('./osaka_traj200/201207{:02d}.csv'.format(d-29), num_loc_o, False)
+        Ytd = data_t[d][np.random.choice(data_t[d].shape[0], 250, replace=False)]
+        Yod = data_o[d][np.random.choice(data_o[d].shape[0], 250, replace=False)]
         Yoe = np.log(Ytd.dot(P.T) + 1e-30)
         score = np.zeros([num_u, num_u])
         print 'Building Score Matrix'
@@ -81,7 +97,7 @@ def dirichlet_process(P, XO, XT):
 
 
 def main():
-    P = np.genfromtxt('./P.csv', delimiter=',')
+    P = np.genfromtxt('./P_tokyo2osaka.csv', delimiter=',')
     XO = np.genfromtxt('/media/fan/HDPC-UT/ZDC/TrainingForMapping/osaka/aggregated_mobilitygraph_large/node.csv', delimiter=',', dtype=np.float)
     XT = np.genfromtxt('/media/fan/HDPC-UT/ZDC/TrainingForMapping/tokyo/aggregated_mobilitygraph_large/node.csv', delimiter=',', dtype=np.float)
     XO /= np.sum(XO, axis=0)
@@ -89,7 +105,7 @@ def main():
     print P.shape
     for i, P, dP in dirichlet_process(P, XO, XT):
         print 'Iteration {}'.format(i)
-        np.savetxt('./dp_rst/P_ITER{}.csv'.format(i), P, delimiter=',')
+        np.savetxt('./dp_rst/P_.csv', P, delimiter=',')
         # np.savetxt('./dp_rst/dP_ITER{}.csv'.format(i), dP, delimiter=',')
 
 
---
+++ discretize_rawdata.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import csv
+import time
+
+# Tokyo
+lat_min_t = 35.5
+lat_max_t = 35.796
+lon_min_t = 139.4
+lon_max_t = 139.9
+# Osaka
+lat_min_o = 34.45
+lat_max_o = 34.85
+lon_min_o = 135.3
+lon_max_o = 135.7
+# Fukuoka
+lat_min_f = 33.48
+lat_max_f = 33.76
+lon_min_f = 130.25
+lon_max_f = 130.7
+# Nagoya
+lat_min_n = 35.00
+lat_max_n = 35.32
+lon_min_n = 136.7
+lon_max_n = 137.1
+
+
+def get_loc_idx(lat, lon, lat_min, lon_min, lat_max, lon_max, num_dislat):
+    dis_lat = int((lat - lat_min) / 0.008)
+    dis_lon = int((lon - lon_min) / 0.010)
+
+    return dis_lat + dis_lon * num_dislat
+
+lat_min = lat_min_t
+lon_min = lon_min_t
+lat_max = lat_max_t
+lon_max = lon_max_t
+num_dislat = int((lat_max - lat_min + 1e-6) / 0.008)
+num_dislon = int((lon_max - lon_min + 1e-6) / 0.010)
+
+for m in xrange(6, 8):
+    for d in xrange(1 , 32):
+        if m == 6 and d == 31:
+            continue
+        user_index= dict({})
+        filename = '/media/fan/HDPC-UT/ZDC/TrainingForMapping/tokyo/sorted/2012{:02d}{:02d}.csv'.format(m, d)
+        init_time = time.mktime(time.strptime('2012-{:02d}-{:02d} 00:00:00'.format(m, d), '%Y-%m-%d %H:%M:%S')) - 600
+        print filename
+        filename_out = '/media/fan/HDPC-UT/ZDC/TrainingForMapping/tokyo/discretized/2012{:02d}{:02d}.csv'.format(m, d)
+        with open(filename_out, 'w') as fout:
+            with open(filename, 'r') as f:
+                for uid_str, lat_str, lon_str, time_str in csv.reader(f):
+                    tstamp = time.mktime(time.strptime(time_str, '%Y-%m-%d %H:%M:%S'))
+                    if tstamp < init_time or tstamp >= init_time + 24 * 3600:
+                        continue
+                    tidx = int((tstamp - init_time) / 1200)
+                    u = int(uid_str)
+                    if u not in user_index:
+                        user_index[u] = len(user_index)
+                    uidx = user_index[u]
+                    lidx = get_loc_idx(float(lat_str), float(lon_str), lat_min, lon_min, lat_max, lon_max, num_dislat)
+                    fout.write('{},{},{}\n'.format(uidx, lidx, tidx))
+
