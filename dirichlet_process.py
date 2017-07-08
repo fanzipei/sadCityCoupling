86ad3e1366cd986d15ecfd6034eef0f713da7f8a
sadsadcool <fanzipei@iis.u-tokyo.ac.jp>
2017-07-09 01:23:28 +0900

    revised diichlet process

--- dirichlet_process.py
+++ dirichlet_process.py
@@ -22,19 +22,19 @@ def load_data(filename, num_loc, is_gaussian):
             uidx = int(uidx_str)
             lidx = int(lidx_str)
             tidx = int(tidx_str)
+            # if lidx >= num_loc:
+                # print lidx
+                # lidx = num_loc - 1
             if uidx not in user_traj:
                 user_traj[uidx] = []
             user_traj[uidx].append((lidx, tidx))
-    for uidx in user_traj:
-        if len(user_traj[uidx]) < 5:
-            del user_traj[uidx]
 
-    return user_traj
+    return dict({k: v for k, v in user_traj.items() if len(v) >= 5})
 
 
 def load_sparse_matrix(data_batch, num_loc, is_gaussian):
-    Ytd = np.zeros([num_time, num_u, num_loc], dtype=np.float) + 0.01
-    for uidx in xrange(data_batch):
+    Ytd = np.zeros([num_time, num_u, num_loc], dtype=np.float) + 1e-5
+    for uidx in xrange(len(data_batch)):
         for lidx_str, tidx_str in data_batch[uidx]:
             lidx = int(lidx_str)
             tidx = int(tidx_str) / 2
@@ -65,51 +65,55 @@ def get_matching(score):
 
 
 def dirichlet_process(P, XO, XT):
-    data_t = []
-    data_o = []
-    for m in xrange(6, 8):
-        for d in xrange(1, 32):
-            if m == 6 and d == 31:
-                continue
-            filename_t = '/media/fan/HDPC-UT/ZDC/TrainingForMapping/tokyo/discretized/2012{:02d}{:02d}.csv'.format(m, d)
-            print 'Reading {}'.format(filename_t)
-            data_t.append(load_data(filename_t, num_loc_t, True))
-            filename_o = '/media/fan/HDPC-UT/ZDC/TrainingForMapping/osaka/discretized/2012{:02d}{:02d}.csv'.format(m, d)
-            print 'Reading {}'.format(filename_o)
-            data_o.append(load_data(filename_o, num_loc_o, False))
-
 
     for i in xrange(max_iter):
+        data_t = []
+        data_o = []
         d = np.random.randint(num_day)
-        data_t_batch = random.sample(data_t[d].values(), num_u)
-        data_o_batch = random.sample(data_o[d].values(), num_u)
-        Ytd = load_sparse_matrix(data_t_batch, num_loc_t, True)
-        Yod = load_sparse_matrix(data_o_batch, num_loc_o, False)
-        Yoe = np.log(Ytd.dot(P.T) + 1e-30)
-        score = np.zeros([num_u, num_u])
-        print 'Building Score Matrix'
-        for uo in xrange(num_u):
-            for ut in xrange(num_u):
-                score[uo, ut] = np.sum(Yod[:, uo, :] * Yoe[:, ut, :])
-        print 'Score Matrix Complete'
-        indexes = get_matching(score)
-        print 'Find out best matching complete'
-        dP = np.zeros([num_loc_o, num_loc_t])
-        for uo, ut in indexes:
-            dP += Yod[:, uo, :].T.dot(Ytd[:, ut, :])
+        m = 0
+        if d >= 30:
+            m = 7
+            d = d - 29
+        else:
+            m = 6
+            d = d + 1
+        filename_t = '/media/fan/HDPC-UT/ZDC/TrainingForMapping/tokyo/discretized/2012{:02d}{:02d}.csv'.format(m, d)
+        print 'Reading {}'.format(filename_t)
+        data_t = load_data(filename_t, num_loc_t, True)
+        filename_o = '/media/fan/HDPC-UT/ZDC/TrainingForMapping/osaka/discretized/2012{:02d}{:02d}.csv'.format(m, d)
+        print 'Reading {}'.format(filename_o)
+        data_o = load_data(filename_o, num_loc_o, False)
 
-        dX = XO - P.dot(XT)
-        dPX = dP.dot(XT)
-        wdP = dX.dot(dPX.T) / ((dX.dot(dX.T) * dPX.dot(dPX.T)) ** 0.5) + 1
-        P += gamma * dP * wdP
-        P /= np.sum(P, axis=0)
-        print 'Update P complete'
+        for _ in xrange(5):
+            data_t_batch = random.sample(data_t.values(), num_u)
+            data_o_batch = random.sample(data_o.values(), num_u)
+            Ytd = load_sparse_matrix(data_t_batch, num_loc_t, True)
+            Yod = load_sparse_matrix(data_o_batch, num_loc_o, False)
+            Yoe = np.log(Ytd.dot(P.T) + 1e-30)
+            score = np.zeros([num_u, num_u])
+            print 'Building Score Matrix'
+            for uo in xrange(num_u):
+                for ut in xrange(num_u):
+                    score[uo, ut] = np.sum(Yod[:, uo, :] * Yoe[:, ut, :])
+            print 'Score Matrix Complete'
+            indexes = get_matching(score)
+            print 'Find out best matching complete'
+            dP = np.zeros([num_loc_o, num_loc_t])
+            for uo, ut in indexes:
+                dP += Yod[:, uo, :].T.dot(Ytd[:, ut, :])
+
+            dX = XO - P.dot(XT)
+            wdP = dX.dot(XT.T) / ((np.outer(np.sum(dX * dX, axis=1), np.sum(XT * XT, axis=1))) ** 0.5) + 1
+            P += gamma * dP * wdP
+            P /= np.sum(P, axis=0)
+            print 'Update P complete'
 
         yield i, P, dP
 
 
 def main():
     P = np.genfromtxt('./P_tokyo2osaka.csv', delimiter=',')
+    print P.shape
     XO = np.genfromtxt('/media/fan/HDPC-UT/ZDC/TrainingForMapping/node_osaka.csv', delimiter=',', dtype=np.float)
     XT = np.genfromtxt('/media/fan/HDPC-UT/ZDC/TrainingForMapping/node_tokyo.csv', delimiter=',', dtype=np.float)
     XO /= np.sum(XO, axis=0)
